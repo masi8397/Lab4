f6548299c579cbff827df9e400e6e8fb57bea22b
masi8397 <marc.simpson@colorado.edu>
2014-09-17 12:06:04 -0600

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,8 @@
 
 public class Hello {
 		public static void main(String[] args){
-			System.out.println("Hello World!"); //Desplay the string.
+			System.out.println("Hello World!");//Desplay the string.
+			System.out.println("How are you?");
 		}
 
 }
