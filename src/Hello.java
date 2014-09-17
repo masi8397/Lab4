6170fa491c754f46077c66b8b57387f3005d0b13
masi8397 <marc.simpson@colorado.edu>
2014-09-17 12:29:28 -0600

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 		public static void main(String[] args){
 			System.out.println("Hello World!");//Desplay the string.
 			System.out.println("How are you?");
+			System.out.println("Favorite color?");
 		}
 
 }
